ff51a31480275a21511540f46a3ea5083a4223e3
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2025-02-17 14:34:40 +0100

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,13 +3,19 @@ import { cors } from "hono/cors";
 
 config();
 
-const allowedOrigins = ["https://fastanime.great-site.net/", "*"];
+const allowedOrigins = ["https://fastanime.great-site.net/"];
 
 const corsConfig = cors({
   allowMethods: ["GET"],
   maxAge: 600,
   credentials: true,
-  origin: allowedOrigins,
+  origin: (origin) => {
+    // Allow specific origins and reject others
+    if (allowedOrigins.includes(origin) || !origin) {
+      return origin;
+    }
+    return new Response("Forbidden", { status: 403 });
+  },
 });
 
 export default corsConfig;
